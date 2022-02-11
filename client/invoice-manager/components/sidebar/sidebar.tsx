30d2f1ebffd8e5a05a0ad6016fc7ed417473484e
adis-tek <67308354+adis-tek@users.noreply.github.com>
2022-02-11 16:22:20 -0600

    sidebar created

---
+++ client/invoice-manager/components/sidebar/sidebar.tsx
@@ -0,0 +1,51 @@
+import type { NextPage } from 'next'
+import styles from "../../styles/sidebar.module.scss"
+
+import Image from "next/image"
+
+const Sidebar: NextPage = () => {
+    return (
+        <div className={styles.sidebar}>
+            <div className={styles.logoContainer}>
+                <Image 
+                src="/logo.png"
+                alt="background-logo"
+                width={103}
+                height={103}
+                className={styles.logo}
+                    />
+            </div>
+            <div className={styles.filler}></div>
+            <div className={styles.switchAndAccount}>
+            <div className={styles.switch}>
+                <div className={styles.ImageHolder}>
+                    <Image 
+                    src="/moon.png"
+                    alt="moon-icon"
+                    width={20}
+                    height={20}
+                    layout="fixed"
+                    className={styles.image}
+                    />
+                </div>
+                </div>
+                <div className={styles.line}></div>
+            <div className={styles.account}>
+                <div className={styles.ImageHolder}>
+                    <Image 
+                    src="/profile-pic.png"
+                    alt="moon-icon"
+                    width={40}
+                    height={40}
+                    layout="fixed"
+                    className={styles.image}
+                    />
+                </div>
+            </div>
+        </div>
+        </div>
+    )
+}
+
+export default Sidebar
+
