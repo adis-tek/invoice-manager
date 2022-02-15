5a0b7b4f6d800706e2b51f2573d3e9405e4d0455
adis-tek <67308354+adis-tek@users.noreply.github.com>
2022-02-14 20:03:20 -0600

    added dynamic invoice page

---
+++ client/invoice-manager/pages/[invoiceId].tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from "next";
+import styles from '../styles/InvoicePage.module.scss'
+
+const InvoicePageId: NextPage = () => {
+    return (
+    <p>Invoice Page ID</p>
+    )
+}
+
+export default InvoicePageId;
