e1532e31dbcfabd78aa4a5e0c5e3e4a4cd8643dd
adis-tek <67308354+adis-tek@users.noreply.github.com>
2022-02-18 23:45:23 -0600

    test routes successful

--- server/db.js
+++ server/db.js
@@ -1,10 +1,13 @@
-const pool = require("pg").Pool;
+const dotenv = require("dotenv");
+const Pool = require("pg").Pool;
+
+dotenv.config();
 
 const pool = new Pool({
-    user: process.env.POOLUSER,
-    password: process.env.POOLPASSWORD,
+    user: `${process.env.POOLUSER}`,
+    password: `${process.env.POOLPASSWORD}`,
     port: process.env.POOLPORT,
-    database: process.env.POOLDATABASE
+    database: `${process.env.POOLDATABASE}`
 });
 
 module.exports = pool;
--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const pool = require("./db");
 
 //middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); //req.body
 
 dotenv.config();
 
@@ -14,11 +14,68 @@ dotenv.config();
 
 //CREATE INVOICES
 
+app.post("/invoices", async (req, res) => {
+    try {
+        const { invoice } = req.body;
+        const newInvoice = await pool.query("INSERT INTO invoice_test (invoice_id, total) VALUES(uuid_generate_v4(), $1) RETURNING *",
+        [invoice]
+        );
+
+        res.json(newInvoice.rows[0]);
+    }   catch (err) {
+        console.log(err.message)
+    }
+});
+
 //READ INVOICES
+
+app.get("/invoices", async(req, res) => {
+    try {
+        const allInvoices = await pool.query("SELECT * FROM invoice_test");
+        res.json(allInvoices.rows);
+    } catch(err) {
+        console.log(err.message)
+    }
+})
+
+//FILTER INVOICES
+
+app.get("/invoices/:id", async(req, res) => {
+    try {
+        const { id } = req.params;
+        const todo = await pool.query("SELECT * FROM invoice_test WHERE total = $1", [id]);
+        res.json(todo.rows[0]);
+    } catch {
+        console.log(err.message);
+    }
+})
 
 //UPDATE INVOICES
 
+app.put("/invoices/:id", async(req, res) => {
+    try {
+        const { id } = req.params;
+        const { newTotal } = req.body;
+        const updateInvoice = await pool.query("UPDATE invoice_test SET total = $1 WHERE total = $2", [newTotal, id]);
+
+        res.json("Invoice Updated")
+    } catch (err) {
+        console.log(err.message);
+        
+    }
+})
+
 //DELETE INVOICES
+
+app.delete("/invoices/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deleteInvoice = await pool.query("DELETE FROM invoice_test WHERE total = $1", [id]);
+        res.json("Invoice deleted");
+    } catch (err) {
+        console.log(err.message);
+    }
+})
 
 
 app.listen(5000, () => {
