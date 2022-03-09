3b01b176306da9579cc2ab225d0a0ee9dd57d9d1
adis-tek <67308354+adis-tek@users.noreply.github.com>
2022-03-09 02:57:09 -0600

    created profile page

--- client/invoice-manager/pages/auth/profile.tsx
+++ client/invoice-manager/pages/auth/profile.tsx
@@ -1,20 +1,20 @@
 import { useState, useEffect, useRef, useCallback } from "react"
 import type { NextPage } from 'next'
 import Link from 'next/link'
-import styles from '../../styles/SignUp.module.scss'
+import styles from '../../styles/Profile.module.scss'
 import Sidebar from "../../components/sidebar/sidebar"
 
-const SignUp: NextPage = () => {
+const Profile: NextPage = () => {
 
     return (
         <div className={styles.mainContainer}>
             <Sidebar />
             <div className={styles.main}>
-            <div className={styles.signUp}>
-            <h1>Welcome to Invoicer</h1>
-            <h2>Create your account</h2>
+            <div className={styles.profile}>
+                <h1>Profile</h1>
+                <div className={styles.line} />
                 <div className={styles.formContainer}>
-                    <div className={styles.emailContainer}>
+                <div className={styles.emailContainer}>
                     <label htmlFor="" className={styles.formLabel}>Email address</label>
                     <input type="email" placeholder="Enter your email" className={styles.formInput} />
                     </div>
@@ -27,20 +27,18 @@ const SignUp: NextPage = () => {
                     <input type="password" placeholder="Confirm your password" className={styles.formInput} />
                     </div>
                     <div className={styles.uploadPhotoContainer}>
-                    <label htmlFor="" className={styles.formLabel}>Upload your profile photo</label>
+                    <label htmlFor="" className={styles.formLabel}>Change your profile photo</label>
                     <input type="file" className={styles.fileFormInput} />
                     </div>
                     <button type="submit" className={styles.submitButton}>
-                        <p className={styles.buttonText}>Sign Up</p>
+                        <p className={styles.buttonText}>Submit Changes</p>
                     </button>
                 </div>
-                <div className={styles.signUpContainer}>
-                    <Link href="/auth/signin"><p>Or <u>Sign In</u></p></Link>
-                </div>
-                </div>
             </div>
-        </div>
+            </div>
+            </div>
     )
+
 }
 
-export default SignUp
+export default Profile;
